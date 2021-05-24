c3f75411fa0a01295f7aecdae59446369d7ff6e8
Vedant Wakalkar <32186983+Karna98@users.noreply.github.com>
2021-05-24 21:40:00 +0530

    Revamp UI (#20)
    * Revamped UI [Dev to Main]

--- background.js
+++ background.js
@@ -13,12 +13,12 @@ let browserDetected = 1;
 let browserAPI;
 
 try {
-	// Firefox
-	browserAPI = browser;
+    // Firefox
+    browserAPI = browser;
 } catch (error) {
-	// Chrome
-	browserDetected = 2;
-	browserAPI = chrome;
+    // Chrome
+    browserDetected = 2;
+    browserAPI = chrome;
 }
 
 // Save Tabs Settings Object
@@ -34,45 +34,45 @@ let LoggerQueue;
  * @param	{String} 	messageType Type of Message
  */
 const logErrorOrSuccess = (messageType, delta) => {
-	// Object containing metadata related to error or success
-	let logObject = {
-		// type 	: {error, success}
-		// message	: {Message}				If type is 'error'
-		// subType 	: {tab, group} 			If type is 'success'
-		// url 		: {url_link} 			If subType is 'tab'
-		// groupName: {Name of the Group} 	If subType is 'group'
-	};
+    // Object containing metadata related to error or success
+    let logObject = {
+        // type 	: {error, success}
+        // message	: {Message}				If type is 'error'
+        // subType 	: {tab, group} 			If type is 'success'
+        // url 		: {url_link} 			If subType is 'tab'
+        // groupName: {Name of the Group} 	If subType is 'group'
+    };
 
-	// If logging enabled.
-	if (saveTabsSettingsObject.logsState) {
-		switch (messageType) {
-			case `error`:
-				logObject = {
-					type: `error`,
-					message: (delta.message || delta)
-				};
-				break;
-			case `newTabCreated`:
-				logObject = {
-					type: `success`,
-					subType: `tab`,
-					url: (delta.status === "loading" ? delta.pendingUrl : delta.title)
-				};
-				break;
-			case `newGroupCreated`:
-				logObject = {
-					type: `success`,
-					subType: `group`,
-					groupName: delta.title
-				};
-				break;
-		}
+    // If logging enabled.
+    if (saveTabsSettingsObject.logsState) {
+        switch (messageType) {
+            case `error`:
+                logObject = {
+                    type: `error`,
+                    message: (delta.message || delta)
+                };
+                break;
+            case `newTabCreated`:
+                logObject = {
+                    type: `success`,
+                    subType: `tab`,
+                    url: (delta.status === "loading" ? delta.pendingUrl : delta.title)
+                };
+                break;
+            case `newGroupCreated`:
+                logObject = {
+                    type: `success`,
+                    subType: `group`,
+                    groupName: delta.title
+                };
+                break;
+        }
 
-		LoggerQueue.unshift({
-			time: new Date().valueOf(),
-			data: logObject
-		});
-	}
+        LoggerQueue.unshift({
+            time: new Date().valueOf(),
+            data: logObject
+        });
+    }
 };
 
 /**
@@ -81,184 +81,184 @@ const logErrorOrSuccess = (messageType, delta) => {
  * @param    {Object} fileContent	Contains list of meta data of tabs and groups (if any).
  */
 const importTabs = (fileContent) => {
-	// Extract list of tabs
-	const listOfTabs = fileContent.tabs;
+    // Extract list of tabs
+    const listOfTabs = fileContent.tabs;
 
-	// Group Tabs Queue to keep track of completion of tabGroups promise.
-	const groupTabsQueue = new Map();
+    // Group Tabs Queue to keep track of completion of tabGroups promise.
+    const groupTabsQueue = new Map();
 
-	// Array of Promises requested.
-	const groupedTabsPromiseArray = [];
+    // Array of Promises requested.
+    const groupedTabsPromiseArray = [];
 
-	for (const tab of listOfTabs) {
-		if (browserDetected == 1 || tab.groupId === undefined || tab.groupId == -1) {
-			// 1. If tabs are opened in FireFox
-			// 2. For Tabs which are not grouped.
+    for (const tab of listOfTabs) {
+        if (browserDetected == 1 || tab.groupId === undefined || tab.groupId == -1) {
+            // 1. If tabs are opened in FireFox
+            // 2. For Tabs which are not grouped.
 
-			// Create new tab and log outcome.
-			browserAPI.tabs
-				.create({
-					url: tab.url,
-					active: false
-				})
-				.then(
-					logErrorOrSuccess.bind(null, `newTabCreated`),
-					logErrorOrSuccess.bind(null, `error`)
-				);
-		} else {
-			// Check if URL to be grouped.
-			if (groupTabsQueue.has(tab.url)) {
-				const getGroup = groupTabsQueue.get(tab.url);
+            // Create new tab and log outcome.
+            browserAPI.tabs
+                .create({
+                    url: tab.url,
+                    active: false
+                })
+                .then(
+                    logErrorOrSuccess.bind(null, `newTabCreated`),
+                    logErrorOrSuccess.bind(null, `error`)
+                );
+        } else {
+            // Check if URL to be grouped.
+            if (groupTabsQueue.has(tab.url)) {
+                const getGroup = groupTabsQueue.get(tab.url);
 
-				getGroup.set(
-					tab.groupId,
-					getGroup.has(tab.groupId) ? getGroup.get(tab.groupId) + 1 : 1
-				);
+                getGroup.set(
+                    tab.groupId,
+                    getGroup.has(tab.groupId) ? getGroup.get(tab.groupId) + 1 : 1
+                );
 
-				groupTabsQueue.set(tab.url, getGroup);
-			} else {
-				groupTabsQueue.set(tab.url, new Map([[tab.groupId, 1]]));
-			}
+                groupTabsQueue.set(tab.url, getGroup);
+            } else {
+                groupTabsQueue.set(tab.url, new Map([[tab.groupId, 1]]));
+            }
 
-			groupedTabsPromiseArray.push(
-				// Create new tab 
-				browserAPI.tabs.create({
-					url: tab.url,
-					active: false,
-				})
-			);
-		}
-	}
+            groupedTabsPromiseArray.push(
+                // Create new tab 
+                browserAPI.tabs.create({
+                    url: tab.url,
+                    active: false,
+                })
+            );
+        }
+    }
 
-	/**
-	 * Store all logs from LoggerQueue to Local Storage.
-	 * @version    1.0.0
-	 */
-	const insertNewLogs = () => {
-		// If logging enabled.
-		if (saveTabsSettingsObject.logsState) {
-			// Check if 'saveTabs' in present or not.
-			browserAPI.storage.local.get(`saveTabs`, (object) => {
+    /**
+     * Store all logs from LoggerQueue to Local Storage.
+     * @version    1.0.0
+     */
+    const insertNewLogs = () => {
+        // If logging enabled.
+        if (saveTabsSettingsObject.logsState) {
+            // Check if 'saveTabs' in present or not.
+            browserAPI.storage.local.get(`saveTabs`, (object) => {
 
-				setTimeout(() => {
-					let FinalLogsQueue = [];
+                setTimeout(() => {
+                    let FinalLogsQueue = [];
 
-					// Latest logged message time.
-					const updated_at = LoggerQueue[0].time;
+                    // Latest logged message time.
+                    const updated_at = LoggerQueue[0].time;
 
-					if (
-						object &&
-						Object.keys(object).length === 0 &&
-						object.constructor === Object
-					) {
-						FinalLogsQueue = LoggerQueue;
-					} else {
-						object.saveTabs.logs.unshift(...LoggerQueue);
-						FinalLogsQueue = object.saveTabs.logs;
-					}
+                    if (
+                        object &&
+                        Object.keys(object).length === 0 &&
+                        object.constructor === Object
+                    ) {
+                        FinalLogsQueue = LoggerQueue;
+                    } else {
+                        object.saveTabs.logs.unshift(...LoggerQueue);
+                        FinalLogsQueue = object.saveTabs.logs;
+                    }
 
-					// Store logs to local storage and then empty LoggerQueue.
-					browserAPI.storage.local.set(
-						{
-							saveTabs: {
-								logs: FinalLogsQueue,
-								updated_at: updated_at,
-							},
-						},
-						() => {
-							LoggerQueue = [];
-						}
-					);
-				}, 1000);
-			});
-		}
-	};
+                    // Store logs to local storage and then empty LoggerQueue.
+                    browserAPI.storage.local.set(
+                        {
+                            saveTabs: {
+                                logs: FinalLogsQueue,
+                                updated_at: updated_at,
+                            },
+                        },
+                        () => {
+                            LoggerQueue = [];
+                        }
+                    );
+                }, 1000);
+            });
+        }
+    };
 
-	/**
-	 * Store all logs from LoggerQueue to Local Storage.
-	 * @version    1.0.0
-	 * @param    {Object} metaData	Meta data of groups with list of respective tabs to be grouped. 
-	 */
-	const moveTabsToGroups = (metaData) => {
-		metaData.forEach((group) => {
-			// Create new Group with tabs listed. 
-			browserAPI.tabs.group({
-				tabIds: group.tabs
-			})
-				.then((delta) => {
-					browserAPI.tabGroups
-						.update(delta, {
-							title: group.title,
-							collapsed: true,
-						})
-						.then(
-							logErrorOrSuccess.bind(null, `newGroupCreated`),
-							logErrorOrSuccess.bind(null, `error`)
-						);
-				}, logErrorOrSuccess.bind(null, `error`));
-		});
+    /**
+     * Store all logs from LoggerQueue to Local Storage.
+     * @version    1.0.0
+     * @param    {Object} metaData	Meta data of groups with list of respective tabs to be grouped. 
+     */
+    const moveTabsToGroups = (metaData) => {
+        metaData.forEach((group) => {
+            // Create new Group with tabs listed. 
+            browserAPI.tabs.group({
+                tabIds: group.tabs
+            })
+                .then((delta) => {
+                    browserAPI.tabGroups
+                        .update(delta, {
+                            title: group.title,
+                            collapsed: true,
+                        })
+                        .then(
+                            logErrorOrSuccess.bind(null, `newGroupCreated`),
+                            logErrorOrSuccess.bind(null, `error`)
+                        );
+                }, logErrorOrSuccess.bind(null, `error`));
+        });
 
-		// To store all logs in local storage.
-		insertNewLogs();
-	};
+        // To store all logs in local storage.
+        insertNewLogs();
+    };
 
-	if (browserDetected == 1 || fileContent.groups === undefined) {
-		// 1. If FireFox.
-		// 2. If no grouped tabs are present.
+    if (browserDetected == 1 || fileContent.groups === undefined) {
+        // 1. If FireFox.
+        // 2. If no grouped tabs are present.
 
-		// To store all logs in local storage.
-		insertNewLogs();
-	} else {
-		// Meta Data of newly created tabs to be grouped.
-		const groupMetaData = new Map();
+        // To store all logs in local storage.
+        insertNewLogs();
+    } else {
+        // Meta Data of newly created tabs to be grouped.
+        const groupMetaData = new Map();
 
-		// To check if all promised are fulfilled.
-		Promise.allSettled(groupedTabsPromiseArray).then((results) => {
-			results.forEach((result) => {
-				// Get URL
-				const url = result.value.url || result.value.pendingUrl;
+        // To check if all promised are fulfilled.
+        Promise.allSettled(groupedTabsPromiseArray).then((results) => {
+            results.forEach((result) => {
+                // Get URL
+                const url = result.value.url || result.value.pendingUrl;
 
-				// URL to be grouped in.
-				const groupsRelatedToURL = groupTabsQueue.get(url);
+                // URL to be grouped in.
+                const groupsRelatedToURL = groupTabsQueue.get(url);
 
-				// Group Id
-				const groupIdForTab = groupsRelatedToURL.keys().next().value;
+                // Group Id
+                const groupIdForTab = groupsRelatedToURL.keys().next().value;
 
-				const groupIdCount = groupsRelatedToURL.get(groupIdForTab);
+                const groupIdCount = groupsRelatedToURL.get(groupIdForTab);
 
-				if (groupIdCount == 1) {
-					((groupsRelatedToURL.size == 1) ?
-						groupTabsQueue.delete(url) :
-						groupsRelatedToURL.delete(groupIdForTab));
-				} else {
-					groupsRelatedToURL.set(groupIdForTab, groupIdCount - 1);
-					groupTabsQueue.set(url, groupsRelatedToURL);
-				}
+                if (groupIdCount == 1) {
+                    ((groupsRelatedToURL.size == 1) ?
+                        groupTabsQueue.delete(url) :
+                        groupsRelatedToURL.delete(groupIdForTab));
+                } else {
+                    groupsRelatedToURL.set(groupIdForTab, groupIdCount - 1);
+                    groupTabsQueue.set(url, groupsRelatedToURL);
+                }
 
-				const metaData = {
-					title: fileContent.groups[groupIdForTab].title,
-				};
+                const metaData = {
+                    title: fileContent.groups[groupIdForTab].title,
+                };
 
-				if (!groupMetaData.has(groupIdForTab)) {
-					metaData[`tabs`] = [result.value.id];
-				} else {
-					metaData[`tabs`] = groupMetaData.get(groupIdForTab).tabs;
-					metaData.tabs.push(result.value.id);
-				}
+                if (!groupMetaData.has(groupIdForTab)) {
+                    metaData[`tabs`] = [result.value.id];
+                } else {
+                    metaData[`tabs`] = groupMetaData.get(groupIdForTab).tabs;
+                    metaData.tabs.push(result.value.id);
+                }
 
-				// Set meta data for respective Group.
-				groupMetaData.set(groupIdForTab, metaData);
+                // Set meta data for respective Group.
+                groupMetaData.set(groupIdForTab, metaData);
 
-				// Log sucessful creation of tab.
-				logErrorOrSuccess(`newTabCreated`, result.value);
+                // Log sucessful creation of tab.
+                logErrorOrSuccess(`newTabCreated`, result.value);
 
-				// If all promises are fulfilled then group tabs.
-				if (groupTabsQueue.size == 0) {
-					moveTabsToGroups(groupMetaData);
-				}
-			});
-		}, logErrorOrSuccess.bind(null, `error`));
-	}
+                // If all promises are fulfilled then group tabs.
+                if (groupTabsQueue.size == 0) {
+                    moveTabsToGroups(groupMetaData);
+                }
+            });
+        }, logErrorOrSuccess.bind(null, `error`));
+    }
 };
 
 /**
@@ -268,33 +268,33 @@ const importTabs = (fileContent) => {
  * @param    {Object} sendResponse	Function to respond recevier with parameters as Message Object.
  */
 const interpretRequest = (message, sender, sendResponse) => {
-	// Check if requesting for import tabs functionality
-	if (message.type === `imported_file_content`) {
-		LoggerQueue = [];
-		saveTabsSettingsObject = message.saveTabsSettings;
-		importTabs(message.data);
-		sendResponse(`Request Submitted Successfully!`);
-	}
+    // Check if requesting for import tabs functionality
+    if (message.type === `imported_file_content`) {
+        LoggerQueue = [];
+        saveTabsSettingsObject = message.saveTabsSettings;
+        importTabs(message.data);
+        sendResponse(`Request Submitted Successfully!`);
+    }
 };
 
 // Sync updated local storage changes
 browserAPI.storage.onChanged.addListener((changes, area) => {
-	if (area === `local` && changes.saveTabsSettings !== undefined) {
-		browserAPI.storage.local.get(`saveTabsSettings`, (object) => {
-			saveTabsSettingsObject = object.saveTabsSettings;
-		});
-	}
+    if (area === `local` && changes.saveTabsSettings !== undefined) {
+        browserAPI.storage.local.get(`saveTabsSettings`, (object) => {
+            saveTabsSettingsObject = object.saveTabsSettings;
+        });
+    }
 });
 
 // Listener to listen message received from saveTabs.js
 browserAPI.runtime.onMessage.addListener(interpretRequest);
 
 if (browserDetected == 1)
-	try {
-		// To toggle sidebar of Firefox browser 
-		browserAPI.browserAction.onClicked.addListener(() => {
-			browserAPI.sidebarAction.toggle();
-		});
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        // To toggle sidebar of Firefox browser 
+        browserAPI.browserAction.onClicked.addListener(() => {
+            browserAPI.sidebarAction.toggle();
+        });
+    } catch (error) {
+        console.log(error);
+    }

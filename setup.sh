800ad2719d2c14b87baa1413824e33bd196d6fd6
karna98 <vedantwakalkar@gmail.com>
2021-06-26 23:00:24 +0530

    Website added

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@
 # 6. icons/
 # 7. saveTab-chrome.css
 mkdir chrome
-cp background.js saveTab.css saveTab-chrome.css saveTab.js saveTab.html chrome/
-cp -R icons chrome
-cp manifest-chrome.json chrome/manifest.json
+cp -R extension/{background.js,saveTab.css,saveTab-chrome.css,saveTab.js,saveTab.html} chrome
+cp -R extension/icons chrome
+cp extension/manifest-chrome.json chrome/manifest.json
 # Creates firefox folder and copy all required files and folders to it.
 # 1. manifest.json (manifest-firefox.json)
 # 2. saveTab.html
@@ -18,6 +18,6 @@ cp manifest-chrome.json chrome/manifest.json
 # 5. background.js
 # 6. icons/
 mkdir firefox
-cp background.js saveTab.css saveTab.js saveTab.html firefox/
-cp -R icons firefox
-cp manifest-firefox.json firefox/manifest.json
+cp -R extension/{background.js,saveTab.css,saveTab.js,saveTab.html} firefox
+cp -R extension/icons firefox
+cp extension/manifest-firefox.json firefox/manifest.json
